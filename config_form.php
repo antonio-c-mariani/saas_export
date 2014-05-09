9e4f837584d4f789869e4d7de14a884f34bb0b42
caiodoneda <caiodoneda@gmail.com>
2014-05-09 11:10:20 -0300

    1 - adicionando novo passo que lista as configurações do bloco junto com o menu inicial para facilitar a visualização e possíveis detecções de problemas de configuração por pessoas que não tem acesso à tela de configurações do plugin 2 - colocando cabeçalho dentro dos passos para facilitar a identificação de dados que são do Moodle e dados que são do SAAS

--- config_form.php
+++ config_form.php
@@ -15,7 +15,7 @@ if(version_compare($current_version, '2.5', '<')) {
 class saas_export_config_form extends moodleform {
 
     function definition() {
-        global $DB, $CFG;
+        global $DB, $CFG, $OUTPUT;
 
         $mform = $this->_form;
 
@@ -34,6 +34,10 @@ class saas_export_config_form extends moodleform {
                 $url_mapping_classes = new moodle_url('/report/saas_export/index.php', array('step'=>2));
                 $url_mapping_polos = new moodle_url('/report/saas_export/index.php', array('step'=>3));
                 $url_send_data = new moodle_url('/report/saas_export/index.php', array('step'=>4));
+                $url_config_table = new moodle_url('/report/saas_export/index.php', array('step'=>6));
+
+                $mform->addElement('html', html_writer::tag('a', get_string('menu_config_table', 'report_saas_export'),
+                                           array('href' => $url_config_table, 'style'=>'display: block; margin-top:0.5cm; margin-left:0.5cm;')));
 
                 $mform->addElement('html', html_writer::tag('a', get_string('menu_map_course_offer', 'report_saas_export'),
                                            array('href' => $url_mapping_courses, 'style'=>'display: block; margin-top:0.5cm; margin-left:0.5cm;')));
@@ -44,13 +48,13 @@ class saas_export_config_form extends moodleform {
                 $mform->addElement('html', html_writer::tag('a', get_string('menu_map_polo', 'report_saas_export'),
                                            array('href' => $url_mapping_polos, 'style'=>'display: block; margin-left:0.5cm;')));
 
-                $mform->addElement('html', html_writer::tag('a', get_string('menu_send_data', 'report_saas_export'), 
+                $mform->addElement('html', html_writer::tag('a', get_string('menu_send_data', 'report_saas_export'),
                                            array('href' => $url_send_data, 'style'=>'display: block; margin-left:0.5cm;')));
                 list($count_new_courses, $count_new_classes) = $saas->update_offers();
-                
+
                 $mform->addElement('html', '<BR>');
                 if ($count_new_courses > 0) {
-                    $mform->addElement('html', html_writer::tag('p', "Há {$count_new_courses} nova(s) oferta(s) de curso a ser(em) mapeada(s).", 
+                    $mform->addElement('html', html_writer::tag('p', "Há {$count_new_courses} nova(s) oferta(s) de curso a ser(em) mapeada(s).",
                                                array('style'=>'font-weight: bold;')));
                 }
                 if ($count_new_classes > 0) {
@@ -64,15 +68,15 @@ class saas_export_config_form extends moodleform {
                 $mform->addElement('hidden', 'step', $step);
                 $mform->setType('step', PARAM_INT);
 
-		$current_version = normalize_version(get_config('', 'release'));
-		if(version_compare($current_version, '2.5', '<')) {
+                $current_version = normalize_version(get_config('', 'release'));
+                if(version_compare($current_version, '2.5', '<')) {
                     $categories = array();
                     $parentlist = array();
                     make_categories_list($categories, $parentlist);
                 } else {
                     $categories = coursecat::make_categories_list();
                 }
-                
+
                 $categories[0] = 'Não relacionar (todos os cursos serão mostrados)';
                 $categories[-1] = 'Ainda não Mapeada';
                 try {
@@ -80,18 +84,20 @@ class saas_export_config_form extends moodleform {
                 } catch (Exception $e) {
                     print_error('ws_error', 'report_saas_export', new moodle_url('/report/saas_export/index.php'), $e->getMessage());
                 }
-                
+
                 try {
                     $saas->save_courses($saas_courses);
                 } catch (Exception $e) {
                     print_error('update_data_error', 'report_saas_export', new moodle_url('/report/saas_export/index.php'), $e->getMessage());
                 }
-                
+
                 $mapped_courses = $saas->get_mapped_courses();
-                
+
                 if (!empty($saas_courses)){
                     $mform->addElement('html', '<h2>' .get_string('title_passo1', 'report_saas_export'). '</h2>');
                     $mform->addElement('html', '<BR>' . get_string('obs_passo1', 'report_saas_export'));
+
+                    $mform->addElement('static', 'title', html_writer::tag('strong', 'SAAS'), html_writer::tag('strong', 'MOODLE'));
 
                     foreach ($saas_courses as $sc) {
                         $mform->addElement('select', 'map['.$sc->uid.']', $sc->nome.' ('.$sc->ano.'-'.$sc->periodo . ')', $categories);
@@ -105,7 +111,7 @@ class saas_export_config_form extends moodleform {
                 } else {
                     $mform->addElement('html', get_string('no_course_offer_from_ws', 'report_saas_export'));
                     $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
-                                                                'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;', 
+                                                                'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
                                                                 'style'=>'display:block; margin-top:1em;')));
                 }
                 break;
@@ -131,7 +137,7 @@ class saas_export_config_form extends moodleform {
                 if (empty($mapped_classes)) {
                     $mform->addElement('html', get_string('no_class_offer_from_ws', 'report_saas_export'));
                     $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
-                                                                'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;', 
+                                                                'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
                                                                 'style'=>'display:block; margin-top:1em;')));
                 } else {
                     $mform->addElement('html', html_writer::tag('h2', get_string('title_passo2', 'report_saas_export')));
@@ -158,6 +164,7 @@ class saas_export_config_form extends moodleform {
                         if (empty($classes_to_show)){
                             $mform->addElement('html', get_string('no_class_offer_from_ws', 'report_saas_export'));
                         } else {
+                            $mform->addElement('static', 'title'.$mapped_course->id, html_writer::tag('strong', 'SAAS'), html_writer::tag('strong', 'MOODLE'));
                             foreach ($classes_to_show as $class){
                                 $class_title = $class->name .' ('. $class->year .'-'.  $mapped_course->period . ')';
                                 $mform->addElement('select', 'map['.$mapped_course->id.']['.$class->saas_id.']', $class_title, $courses);
@@ -187,13 +194,13 @@ class saas_export_config_form extends moodleform {
                     if (empty($mapped_courses)) {
                         $mform->addElement('html', get_string('no_mapped_courses_offer', 'report_saas_export'));
                         $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
-                                                                   'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;', 
+                                                                   'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
                                                                    'style'=>'display:block; margin-top:1em;')));
                     } else if (empty($mapped_classes)) {
                         $mform->addElement('html', get_string('no_mapped_classes_offer', 'report_saas_export'));
-			$mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
-			                                            'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;', 
-								    'style'=>'display:block; margin-top:1em;')));
+                        $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
+                                                                    'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
+                                                                    'style'=>'display:block; margin-top:1em;')));
                     } else {
                         $mform->addElement('html', html_writer::tag('h2', get_string('title_passo3', 'report_saas_export')));
                         $mform->addElement('html', get_string('obs_passo3', 'report_saas_export'));
@@ -239,23 +246,23 @@ class saas_export_config_form extends moodleform {
                                            array('style'=>'display: block; margin-top:0.5cm;')));
 
                 $total_of_users = $saas->get_total_users_to_send();
-		
+
                 if(empty($total_of_users)){
                     $mform->addElement('hidden', 'step', 0);
                     $mform->setType('step', PARAM_INT);
-                    
+
                     $mform->addElement('html', get_string('no_data_to_export','report_saas_export'));
                     $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
-                                                    'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;', 
+                                                    'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
                                                     'style'=>'display:block; margin-top:1em;')));
 
-		} else {
+                } else {
                     $mform->addElement('hidden', 'step', $step);
                     $mform->setType('step', PARAM_INT);
-                    
+
                     $mform->addElement('html', html_writer::tag('p', get_string('obs_passo4', 'report_saas_export'),
                                                array('style'=>'display: block; margin-top:0.5cm;')));
-    
+
                     $mform->addElement('html', html_writer::tag('p', get_string('total_users_to_send', 'report_saas_export') . $total_of_users,
                                                array('style'=>'display: block; margin-top:0.5cm;')));
 
@@ -282,9 +289,9 @@ class saas_export_config_form extends moodleform {
                             $l = array('&nbsp &nbsp' . $class_names[$class_offer]);
                             foreach (saas::$role_names as $r){
                                 if (isset($counts[$r])){
-                                    $l[] = $counts[$r]; 
+                                    $l[] = $counts[$r];
                                 } else {
-                                    $l[] = 0; 
+                                    $l[] = 0;
                                 }
                             }
                             $line[] = $l;
@@ -307,6 +314,68 @@ class saas_export_config_form extends moodleform {
                                            array('style'=>'display: block; margin-top:0.5cm;')));
 
                 $this->add_action_buttons(false, 'Finalizar');
+
+                break;
+
+           case 6:
+                $mform->addElement('hidden', 'step', 0);
+                $mform->setType('step', PARAM_INT);
+
+                $mform->addElement('html', html_writer::tag('h2', get_string('config', 'report_saas_export'),
+                                           array('style'=>'display: block; margin-top:0.5cm;')));
+
+                $table = new html_table();
+                $table->head = array('Nome', 'Valor');
+
+                $config = $saas->config;
+                unset($config->api_key);
+
+                $data = array();
+                $data[] = array('name' => get_string('ws_url', 'report_saas_export') . $OUTPUT->help_icon('ws_url', 'report_saas_export'),
+                                'value' => $config->ws_url);
+
+                $data[] = array('name' => get_string('course_name_default', 'report_saas_export'). $OUTPUT->help_icon('course_name_default', 'report_saas_export'), 
+                                'value' => $config->course_name_default);
+                $data[] = array('name' => get_string('user_id_field', 'report_saas_export') . $OUTPUT->help_icon('user_id_field', 'report_saas_export'),
+                                'value' => $config->user_id_field);
+
+                $cpf_field_help =  $OUTPUT->help_icon('cpf', 'report_saas_export');
+                $data[] = array('name' => get_string('cpf_field', 'report_saas_export', 'estudantes') . $cpf_field_help,
+                                'value' => $config->cpf_student_field);
+                $data[] = array('name' => get_string('cpf_field', 'report_saas_export', 'tutores a distância') . $cpf_field_help,
+                                'value' => $config->cpf_tutor_field);
+                $data[] = array('name' => get_string('cpf_field', 'report_saas_export', 'tutores presênciais') . $cpf_field_help,
+                                'value' => $config->cpf_tutor_polo_field);
+                $data[] = array('name' => get_string('cpf_field', 'report_saas_export', 'professores') . $cpf_field_help,
+                                'value' => $config->cpf_teacher_field);
+
+                $name_field_help = $OUTPUT->help_icon('profile_field_name', 'report_saas_export');
+                $data[] = array('name' => get_string('name_field', 'report_saas_export', 'estudantes') . $name_field_help,
+                                'value' => get_string($config->name_student_field, 'report_saas_export'));
+                $data[] = array('name' => get_string('name_field', 'report_saas_export', 'tutores a distância') . $name_field_help,
+                                'value' => get_string($config->name_tutor_field, 'report_saas_export'));
+                $data[] = array('name' => get_string('name_field', 'report_saas_export', 'tutores presênciais') . $name_field_help,
+                                'value' => get_string($config->name_tutor_polo_field, 'report_saas_export'));
+                $data[] = array('name' => get_string('name_field', 'report_saas_export', 'professores') . $name_field_help,
+                                'value' => get_string($config->name_teacher_field, 'report_saas_export'));
+
+                $user_role_help = $OUTPUT->help_icon('roles', 'report_saas_export');
+                $data[] = array('name' => get_string('user_role', 'report_saas_export', 'estudantes') . $user_role_help,
+                                'value' => $saas->serialize_role_names($config->student_role));
+                $data[] = array('name' => get_string('user_role', 'report_saas_export', 'tutores a distância') . $user_role_help,
+                                'value' => $saas->serialize_role_names($config->tutor_role));
+                $data[] = array('name' => get_string('user_role', 'report_saas_export', 'tutores presênciais') . $user_role_help,
+                                'value' => $saas->serialize_role_names($config->tutor_polo_role));
+                $data[] = array('name' => get_string('user_role', 'report_saas_export', 'professores') . $user_role_help,
+                                'value' => $saas->serialize_role_names($config->teacher_role));
+
+                $table->data = $data;
+
+                $mform->addElement('html', html_writer::table($table));
+
+                $mform->addElement('html', html_writer::tag('input', '', array('class'=>'btn-cancel', 'name'=>'cancel', 'value'=> 'Voltar',
+                                                'type'=>'submit', 'onclick'=>'skipClientValidation = true; return true;',
+                                                'style'=>'display:block; margin-top:1em;')));
 
                 break;
         }
