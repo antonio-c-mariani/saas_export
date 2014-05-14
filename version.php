f97126582ae968e509f8906e801a183712ea6210
caiodoneda <caiodoneda@gmail.com>
2014-05-14 16:34:32 -0300

    nova versão do plugin (2014050908)

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014040307;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014050908;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400;        // Requires this Moodle version
 $plugin->component = 'report_saas_export'; // Full name of the plugin (used for diagnostics)
