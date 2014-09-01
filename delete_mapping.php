8a11611cbc1be89d3c90cedf74115ec9737d60e3
Daniel Neis Araujo <daniel@setic.ufsc.br>
2014-09-01 18:30:29 -0300

    reindent + code style + remove unnecessary require of saas class

--- delete_mapping.php
+++ delete_mapping.php
@@ -8,12 +8,12 @@ $action = optional_param('action', 'delete_one', PARAM_TEXT);
 
 switch ($action) {
     case 'delete_one':
-        $DB->delete_records('saas_map_course', array('courseid'=>$id, 'oferta_disciplina_id'=>$uid));
+        $DB->delete_records('saas_map_course', array('courseid' => $id, 'oferta_disciplina_id' => $uid));
         break;
     case 'delete_many_offers':
-        $DB->delete_records('saas_map_course', array('courseid'=>$id));
+        $DB->delete_records('saas_map_course', array('courseid' => $id));
         break;
     case 'delete_many_courses':
-        $DB->delete_records('saas_map_course', array('oferta_disciplina_id'=>$uid));
+        $DB->delete_records('saas_map_course', array('oferta_disciplina_id' => $uid));
         break;
 }
--- save_mapping.php
+++ save_mapping.php
@@ -1,18 +1,40 @@
 <?php
-    define('AJAX_SCRIPT', true);
-    require_once '../../config.php';
-    require_once($CFG->dirroot . '/report/saas_export/classes/saas.php');
+define('AJAX_SCRIPT', true);
+require_once '../../config.php';
 
-    $uid = required_param('uid', PARAM_TEXT);
-    $id = required_param('id', PARAM_TEXT);
+$uid = required_param('uid', PARAM_TEXT);
+$id = required_param('id', PARAM_TEXT);
 
-    $saas = new saas();
-    $mapping_type = $saas->config->course_mapping;
+$mapping_type = get_config('report_saas_export', 'course_mapping');
 
-    switch ($mapping_type) {
-        case 'one_to_one':
-            if ($record = $DB->get_record('saas_map_course', array('oferta_disciplina_id' => $uid))) {
-                $record->courseid = $id;
+switch ($mapping_type) {
+    case 'one_to_one':
+        if ($record = $DB->get_record('saas_map_course', array('oferta_disciplina_id' => $uid))) {
+            $record->courseid = $id;
+            $DB->update_record('saas_map_course', $record);
+        } else {
+            $record = new stdClass();
+            $record->courseid = $id;
+            $record->oferta_disciplina_id = $uid;
+            $DB->insert_record('saas_map_course', $record);
+        }
+
+        break;
+    case 'many_to_one':
+        $record = new stdClass();
+        $record->courseid = $id;
+        $record->oferta_disciplina_id = $uid;
+        $DB->insert_record('saas_map_course', $record);
+        break;
+    case 'one_to_many':
+        if ($uid == -1) {
+            $record = new stdClass();
+            $record->courseid = $id;
+            $record->oferta_disciplina_id = $uid;
+            $DB->insert_record('saas_map_course', $record);
+        } else {
+            if ($record = $DB->get_record('saas_map_course', array('courseid' => $id, 'oferta_disciplina_id' => -1))) {
+                $record->oferta_disciplina_id = $uid;
                 $DB->update_record('saas_map_course', $record);
             } else {
                 $record = new stdClass();
@@ -20,31 +42,6 @@
                 $record->oferta_disciplina_id = $uid;
                 $DB->insert_record('saas_map_course', $record);
             }
-
-            break;
-        case 'many_to_one':
-            $record = new stdClass();
-            $record->courseid = $id;
-            $record->oferta_disciplina_id = $uid;
-            $DB->insert_record('saas_map_course', $record);
-            break;
-        case 'one_to_many':
-            if ($uid == -1) {
-                $record = new stdClass();
-                $record->courseid = $id;
-                $record->oferta_disciplina_id = $uid;
-                $DB->insert_record('saas_map_course', $record);
-            } else {
-                if ($record = $DB->get_record('saas_map_course', array('courseid'=>$id, 'oferta_disciplina_id'=>-1))) {
-                    $record->oferta_disciplina_id = $uid;
-                    $DB->update_record('saas_map_course', $record);
-                } else {
-                    $record = new stdClass();
-                    $record->courseid = $id;
-                    $record->oferta_disciplina_id = $uid;
-                    $DB->insert_record('saas_map_course', $record);
-                }
-            }
-           break;
-    }
-?>
+        }
+       break;
+}
