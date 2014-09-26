048d6481adf4914b9af6455fcb2b34944d656004
Antonio Carlos Mariani <a.c.mariani@inf.ufsc.br>
2014-09-26 17:16:11 -0300

    Atualizando versão

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014092100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014092600;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400;        // Requires this Moodle version
 $plugin->component = 'report_saas_export'; // Full name of the plugin (used for diagnostics)
